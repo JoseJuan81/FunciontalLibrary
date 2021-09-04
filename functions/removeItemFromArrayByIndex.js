e29937b9912c3b3877d8cb6e66ab49187d7465ca
José Juan Domínguez López <pe.dominguezjosejuan@gmail.com>
2021-09-04 11:20:54 -0500

    Update removeItemFromArrayByIndex.js
    Evito mutar el argumento ( arreglo )

--- functions/removeItemFromArrayByIndex.js
+++ functions/removeItemFromArrayByIndex.js
@@ -1,10 +1,11 @@
 /**
- *	removeItemFromArrayByIndex
+ *	@description remueve elemento de un arreglo usando el índice
  *	@param { number } index - ubicación de elemento en arreglo
  *	@param { array } arr - arreglo de elementos
  *	@return { array } - array con un elemento menos (el que estaba en posición index)
  */
 export default (index, arr) => {
-	arr.splice(index, 1);
-	return arr;
+	const localArr = [...arr]
+	localArr.splice(index, 1);
+	return [...localArr];
 };
